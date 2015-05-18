c9517cd61ed2afde78d25b22c4bcd055b04ddcfb
dingfeng <dingfeng12345@gmail.com>
2015-05-18 14:58:34 +0800

    test  2

--- Classes/AudioStreamer.h
+++ Classes/AudioStreamer.h
@@ -20,6 +20,8 @@
 //  3. This notice may not be removed or altered from any source
 //     distribution.
 //   20150515  got  test    from  sourcetree
+//   20150518  got  test    from  sourcetree
+
 
 #if TARGET_OS_IPHONE			
 #import <UIKit/UIKit.h>
